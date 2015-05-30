f104ddf7a1f6a6f44178cc6e3bd0a3c1b642987b
Tim Smith <tim@cozy.co>
2015-05-30 13:16:09 -0700

    Update version library

--- lib/sensu-plugins-greylog.rb
+++ lib/sensu-plugins-greylog.rb
@@ -1,7 +1,15 @@
+
+require 'sensu-plugins-greylog/version'
+
+# Load the defaults
+
 #
-# Set gem version
+# Default class
 #
 module SensuPluginsGreylog
-  # Gem version
-  VERSION = '0.0.1.alpha.1'
+  class << self
+  end
+
+  class << self
+  end
 end
